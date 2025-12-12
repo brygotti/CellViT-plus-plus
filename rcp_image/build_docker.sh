b4aa29f54fb72499eea09942d99672958c07d5df
Bryan Gotti <bry.gotti@outlook.com>
2025-12-12 11:09:12 +0100

    Make image take less space by squashing layers

--- rcp_image/build_docker.sh
+++ rcp_image/build_docker.sh
@@ -1,1 +1,1 @@
-docker buildx build --tag=registry.rcp.epfl.ch/course-cs-433-group08/gotti:py310 --platform=linux/amd64 --push .
+docker buildx build --tag=registry.rcp.epfl.ch/course-cs-433-group08/gotti:latest --platform=linux/amd64 --push .
